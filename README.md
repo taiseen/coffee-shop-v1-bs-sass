8d10e91b57393c02581206b0a2c1f3c0127c47d5
taiseen <taiseen.cse@gmail.com>
2022-01-21 13:05:49 +0600

    header + home ==> OK

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 21 - Jan - 2022 
 
-# Project Title | [Live-Link](https://taiseen.github.io/repo-name)
+# Project Title | [Live-Link](https://taiseen.github.io/coffee-shop-v1-bs-sass)
 
-## Inspire & Learning From ==> [this link](https://)
+## Inspire & Learning From ==> [this link](https://youtu.be/kCp-iWslG4o)
 
 ## Learning Context
 - SASS ==> variable , nesting , @mixing + @include 
-- Side Toggle Menu Button 
+- Side Toggle Menu Button
+- Clicking - Login Form
+- Window Scroll Effect
 - Div Opacity Hover Effect 
 - Responsiveness
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,42 @@
 // 21 - Jan - 2022 
 
-const data = document.getElementById('data');
-const data = document.querySelector('.data');
+const loggingBtn = document.getElementById('login-btn');
+const closeBtn = document.getElementById('closeBtn');
+const formDisplay = document.querySelector('.login-form');
+const header = document.querySelector('.header');
+const menuBtn = document.getElementById('menu-btn');
+const nav = document.querySelector('.nav');
+
+
+// Login Form Open
+loggingBtn.addEventListener('click', () => {
+    formDisplay.classList.add('userClick');
+});
+
+
+// Login Form Close
+closeBtn.addEventListener('click', () => {
+    formDisplay.classList.remove('userClick');
+});
+
+
+// Menu Btn Toggling 
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('fa-times');
+    nav.classList.toggle('userClick');
+});
+
+
+
+window.addEventListener('scroll', () => {
+    // formDisplay.classList.remove('userClick');
+    menuBtn.classList.remove('fa-times');
+    nav.classList.remove('userClick');
+
+    /* By scrolling HEADER background color change */
+    if (window.scrollY > 0) {
+        header.classList.add('userScroll');
+    } else {
+        header.classList.remove('userScroll');
+    }
+});
